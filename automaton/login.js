72cddcde77d02ed8a60631d484608eb948b6a9da
Ahmad Shahzad <>
2023-03-16 01:46:59 +0500

    Created the logic for puppeteer to open the Twitter login page.

--- automaton/login.js
+++ automaton/login.js
@@ -1,5 +1,34 @@
+const puppeteer = require('puppeteer');
+
 const login = async () => {
-    return true;
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized', '--disable-notifications'],
+        userDataDir: './userData',
+    });
+
+    const page = await browser.newPage();
+
+    try {
+        await page.goto('https://www.twitter.com/login');
+        await page.waitForNetworkIdle();
+
+        await browser.close();
+        return {
+            status: true,
+            code: 0,
+            err: null,
+        };
+    } catch (err) {
+        await browser.close();
+        console.error('There was an error loggin in to Twitter.', err);
+        return {
+            status: false,
+            code: 999,
+            err,
+        };
+    }
 };
 
 module.exports = login;
