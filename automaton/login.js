f4b46415ae0d144b009ac0eb5b291ba61f108f46
Ahmad Shahzad <>
2023-03-16 01:49:42 +0500

    Get the username and password from the env and then check if the user is already logged in.

--- automaton/login.js
+++ automaton/login.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
 
 const login = async () => {
+    const username = process.env.LOGIN_USER;
+    const password = process.env.LOGIN_PASS;
+
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
@@ -13,6 +16,15 @@ const login = async () => {
     try {
         await page.goto('https://www.twitter.com/login');
         await page.waitForNetworkIdle();
+
+        if ((await page.url()) !== 'https://twitter.com/i/flow/login') {
+            await browser.close();
+            return {
+                status: true,
+                code: 1,
+                err: null,
+            };
+        }
 
         await browser.close();
         return {
