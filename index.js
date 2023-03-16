736131b8256f2b5731904bf578818165387bc372
Ahmad Shahzad <>
2023-03-17 04:40:01 +0500

    Using body-parser to manage form inputs.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 45000;
@@ -11,6 +12,9 @@ nunjucks.configure(path.join(__dirname, 'views'), { express: app, autoescape: tr
 app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const router = {
     home: require(path.join(__dirname, 'routes', 'index')),
