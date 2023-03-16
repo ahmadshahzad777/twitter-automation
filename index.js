b44efb785d61910bae57977515c6898f2e9ea5a2
Ahmad Shahzad <>
2023-03-16 16:26:45 +0500

    Separated the express routes into separate modules.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 45000;
 
-app.get('/', (req, res) => {
-    res.send(`Hello, ${process.env.LOGIN_USER}!`);
-});
+const router = {
+    home: require(path.join(__dirname, 'routes', 'index')),
+};
+
+app.use('/', router.home);
 
 app.listen(port);
