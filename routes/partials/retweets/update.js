b7b0e95c44d7e8795f93b0acebcb25915489ab6b
Ahmad Shahzad <>
2023-03-17 06:41:09 +0500

    Created the logic for updating a retweet.

---
+++ routes/partials/retweets/update.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+module.exports = router;
+
+const db = require(path.join(__dirname, '..', '..', '..', 'db'));
+
+router.post('/', (req, res) => {
+    let id = req.body.id;
+    let link = Buffer.from(req.body.link).toString('base64');
+    let date = req.body.time;
+    let priority = req.body.priority;
+
+    let INSERT_RECORD = 'UPDATE INTO retweets (link, time, priority) VALUES (';
+    INSERT_RECORD += `"${link}", `;
+    INSERT_RECORD += `"${date}", `;
+    INSERT_RECORD += priority;
+    INSERT_RECORD += ')';
+
+    let UPDATE_RECORD = 'UPDATE retweets SET ';
+    UPDATE_RECORD += `link = "${link}", `;
+    UPDATE_RECORD += `time = "${date}", `;
+    UPDATE_RECORD += `priority = ${priority} `;
+    UPDATE_RECORD += `WHERE id = ${id}`;
+
+    db.run(UPDATE_RECORD, (err) => {
+        if (err) {
+            console.error('There was an error updating the retweet schedule.', err);
+            return res.redirect('/');
+        }
+
+        return res.redirect('/retweets');
+    });
+});
--- routes/retweets.js
+++ routes/retweets.js
@@ -9,7 +9,7 @@ const retweets = {
     now: require(path.join(__dirname, 'partials', 'retweets', 'now')),
     // order: require(path.join(__dirname, 'partials', 'retweets', 'order')),
     create: require(path.join(__dirname, 'partials', 'retweets', 'create')),
-    // update: require(path.join(__dirname, 'partials', 'retweets', 'update')),
+    update: require(path.join(__dirname, 'partials', 'retweets', 'update')),
     delete: require(path.join(__dirname, 'partials', 'retweets', 'delete')),
     // reorder: require(path.join(__dirname, 'partials', 'retweets', 'reorder')),
 };
@@ -18,6 +18,6 @@ router.use('/', retweets.get);
 router.use('/now', retweets.now);
 // router.use('/order', retweets.order);
 router.use('/create', retweets.create);
-// router.use('/update', retweets.update);
+router.use('/update', retweets.update);
 router.use('/delete', retweets.delete);
 // router.use('/reorder', retweets.reorder);
